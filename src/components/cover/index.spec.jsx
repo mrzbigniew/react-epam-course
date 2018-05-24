299e124a09a2b1a745ee42bc1e7b222901c0ad60
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-05-24 21:41:23 +0200

    task5: tests update

--- src/components/cover/index.spec.jsx
+++ src/components/cover/index.spec.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import configureStore from 'redux-mock-store'
 
 import Cover from './index';
 
-
-jest.mock('../spinner', () => 'Spinner');
+// jest.mock('../spinner', () => 'Spinner');
 
 describe('Cover', () => {
-    let store;
-    beforeEach(() => {
-        store = {
+
+    it('renders', () => {
+        const store = {
             getState: () => ({
                 spinner: false
             }),
             dispatch: () => {},
             subscribe: () => {}
         }
-    });
-
-    it('renders', () => {
         const component = renderer.create(<Cover store={store} />);
         expect(component.toJSON()).toMatchSnapshot();
+    });
 
-        store.getState = () => ({
-            spinner: true
-        });
-        const component2 = renderer.create(<Cover store={store} />);
-        expect(component2.toJSON()).toMatchSnapshot();
+    it('sets display to flex if state spinner is true', () => {
+        const store = {
+            getState: () => ({
+                spinner: true
+            }),
+            dispatch: () => {},
+            subscribe: () => {}
+        }
+        const wrapper = mount(<Cover  store={store} />);
+        expect(wrapper.find('div').at(0).props().style.display).toEqual('flex');
+    });
+
+    it('sets display to none if state spinner is true', () => {
+        const store = {
+            getState: () => ({
+                spinner: false
+            }),
+            dispatch: () => {},
+            subscribe: () => {}
+        }
+        const wrapper = mount(<Cover  store={store} />);
+        expect(wrapper.find('div').at(0).props().style.display).toEqual('none');
     });
 });
